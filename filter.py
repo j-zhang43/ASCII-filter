18e18e2ec46d5a854f7bba446f88178037a1c6c4
Jason Zhang <jaszhang43@gmail.com>
2025-11-20 14:37:25 -0500

    Changed to texture filter

---
+++ filter.py
@@ -0,0 +1,15 @@
+import os
+import cv2
+import sys
+
+
+def process_texture():
+    cv2.imshow
+
+def main():
+    if len(sys.argv0) != 2:
+        print("Usage: filter.py [img_path]")
+
+
+
+    cv2.imread(f"{os.getcwd}")
